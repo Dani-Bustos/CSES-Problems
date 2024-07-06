b528cb7ca508e11ca434a80e16c5d210b94bbc38
Dani-Bustos <danielbustoslangmandel@gmail.com>
2024-07-06 14:00:27 -0300

    Progreso en Monsters

--- GraphProblems/Monsters.cpp
+++ GraphProblems/Monsters.cpp
@@ -3,76 +3,88 @@ using namespace std;
 #define fore(i,a,b) for(int i = a; i< b; i++)
 typedef long long ll ;
 const ll INF = 1e15;
+int n, m;
 
-struct ComparePairs {
-    bool operator()(const pair<ll, int>& p1, const pair<ll, int>& p2) {
-        // Compare first elements; if equal, compare second elements
-        return p1.first > p2.first || (p1.first == p2.first && p1.second > p2.second);
+
+vector<vector<char>> mat;
+
+struct Node{
+    int x,y,lvl;
+    Node(int y_, int x_, int lvl_){
+        x = x_ ; y = y_;lvl = lvl_;
     }
 };
 
-
-struct edge{
-    int vertex;
-    ll cost;
-    edge(int v=-1,ll c =-1){
-        vertex= v;
-        cost =c;
-    }
-};
-
-struct digraph{
-    vector<list<edge>> adyacencias;
-
-    digraph(int cantVertices){
-        adyacencias.resize(cantVertices);
-    }
-
-    void addEdge(int v,int w,ll weight){
-        adyacencias[v].push_back(edge(w,weight));
-    }
-    
-    vector<ll> Djikstra(int root){
-        vector<bool> visitado(adyacencias.size(),false);
-        vector<ll> d(adyacencias.size(),INF);
-        d[root] = 0;
-        
-        priority_queue<pair<ll,int>,vector<pair<ll,int>> ,ComparePairs> q;
-        q.push({0,root});
-        while(!q.empty()){
-            int v;ll act; tie(act,v) = q.top(); q.pop();
-            
-            if(visitado[v]) continue;
-            visitado[v] = true;
-            
-            for(edge w: adyacencias[v]){
-                if(not visitado[w.vertex] && d[w.vertex] > w.cost + act){ 
-                    d[w.vertex] = w.cost + act;
-                    //El heap default es max
-                    if(not visitado[w.vertex]) q.push({d[w.vertex],w.vertex});   
-                }
-            }
-        }
-        return d;
-    }
-
-};
+bool isValid(int y, int x){
+    //Capaz que hay que pedir que no sea monstruo
+    return (x != m && x != -1 && y != n && y != -1 && mat[y][x] != '#');
+}
 
 
-
-
+bool esBorde(int i,int j){
+    return (j == 0 || j == m - 1) || (i == 0 || i == n -1);
+}
 
 int main(){
-    int n, m;
+   
     cin >> n >> m;
-    digraph G(n);
-    fore(i,0,m){
-        int a,b; ll c;
-        cin >> a >> b >> c;
-        G.addEdge(a-1,b-1,c);
+    mat.resize(n,vector<char>(m));
+    
+    vector<pair<int,int>> bordes;
+    char temp;
+    vector<pair<int,int>> monstruos;
+    pair<int,int> start;
+   
+    fore(i,0,n){
+        fore(j,0,m){
+            cin >> temp;
+            mat[i][j] = temp;
+            if(esBorde(i,j) && temp == '.') {
+                bordes.push_back({i,j});
+            }else if(temp == 'M') monstruos.push_back({i,j});
+            else if(temp == 'A') start = {i,j};
+
+        }
+       
     }
-    vector<ll> res = G.Djikstra(0);
-    for(ll num : res){
-        cout << num << " ";
+    
+    vector<vector<int>> D(n,vector<int>(m)); vector<vector<pair<int,int>> > P(n,vector<pair<int,int>>(m));
+    //BFS desde el inicio a todos
+    D[start.first][start.second] = 0;
+    queue<Node> q;
+    Node inicio(start.first,start.second,0);
+    q.push(inicio);
+    while(!q.empty()){
+        Node act = q.front(); q.pop();
+        //Abajo
+        if(isValid(act.y + 1,act.x) && D[act.y + 1][act.x]){
+            D[act.y + 1][act.x] = act.lvl + 1;
+            P[act.y + 1][act.x] = {act.y,act.x};
+            q.push(Node(act.y+1,act.x, act.lvl + 1));
+        }
+        //Arriba
+        if(isValid(act.y -1 ,act.x) && D[act.y - 1][act.x]){
+            D[act.y - 1][act.x] = act.lvl + 1;
+            P[act.y - 1][act.x] = {act.y,act.x};
+            q.push(Node(act.y-1,act.x, act.lvl + 1));
+        }
+        //Derecha
+        if(isValid(act.y ,act.x + 1) && D[act.y][act.x + 1]){
+            D[act.y ][act.x + 1] = act.lvl + 1;
+            P[act.y ][act.x + 1] = {act.y,act.x};
+            q.push(Node(act.y,act.x + 1, act.lvl + 1));
+        }
+        //Izqueierda
+        if(isValid(act.y,act.x -1) && D[act.y ][act.x-1]){
+            D[act.y ][act.x - 1] = act.lvl + 1;
+             P[act.y ][act.x - 1] = {act.y,act.x};
+            q.push(Node(act.y,act.x - 1, act.lvl + 1));
+        }
+
     }
+    //BFS desde el super monstruo
+    vector<vector<int>> M(n,vector<int>(m));
+    
+
+
 }
