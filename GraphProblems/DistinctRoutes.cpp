2a2ed87e3e797ca6b36ad3bf41edcdec512ef4c2
Dani-Bustos <danielbustoslangmandel@gmail.com>
2024-07-06 18:15:46 -0300

    El utlimo de flujo hay que darle un ID a las aristas

--- GraphProblems/DistinctRoutes.cpp
+++ GraphProblems/DistinctRoutes.cpp
@@ -1,57 +1,62 @@
-
-    
- #include<bits/stdc++.h>
-#define fore(i,a,b) for(int i = a; i< b;i++)
+#include<bits/stdc++.h>
+#define fore(i,a,b) for(int i = a; i < b;i++)
 using namespace std;
+typedef long long ll; const int INF = 1e9;
 
-const int INF = 1e9;
-int N, M; //N:= Numero de remeras, M:= Cantidad de voluntarios 
-//Vamos a trabajar directamente con la red residual
 
 struct flow_digraph{
-    vector<vector<int>> rRes;
-    vector<list<int>> ogAdy;
+    
+    vector<vector<int>> flujo;
+    vector<vector<int>> capacidad;
     vector<list<int>> adyacencias;
+    vector<list<int>> adyacenciasOG;
     vector<int> caminoAumento;
+    vector<int> aristas;
+   
     
     
     //Nuestra fuente es el primero, el sumidero el ultimo
-    flow_digraph(int cantVertices){
+    flow_digraph(int cantVertices, int cantAristas = 0){
+        aristas.resize(cantAristas);
         adyacencias.resize(cantVertices);
-        rRes.resize(cantVertices,vector<int>(cantVertices,0));
-        ogAdy.resize(cantVertices);
+        adyacenciasOG.resize(cantVertices);
+        flujo.resize(cantVertices,vector<int>(cantVertices,0));
+        capacidad.resize(cantVertices,vector<int>(cantVertices,0));
     }
 
-    void addPair(int p1, int p2){
-        //Estamos construyendo la red residual
-        adyacencias[p1].push_back(p2);
-        adyacencias[p2].push_back(p1);
-        ogAdy[p1].push_back(p2);
-        rRes[p1][p2] = 1;
-        rRes[p2][p1] = 0;
+    void addEdge(int v , int w, int cap){
+        //Construimos el residual, añadimos de los dos lados
+        adyacencias[v].push_back(w);
+        adyacencias[w].push_back(v);    
+        capacidad[v][w]  = cap;
+        adyacenciasOG[v].push_back(w);
         
-    
     }
+    
     
     //Algoritmo de Edmond-Karps
     int maxFlow(){
-        int flujo = 0;
         while(hasAugmenthPath()){
             int cuelloBotella = findBottleNeck();
-            flujo+= cuelloBotella;
             fore(i,0,caminoAumento.size()-1){
-                //Subo el rRes de la arista si esta en el grafo
+                //Subo el flujo de la arista si esta en el grafo
                 
-                rRes[caminoAumento[i]][caminoAumento[i+1]] -= cuelloBotella;
-               
-                //Bajo el rRes de la arista si no esta
-                rRes[caminoAumento[i+1]][caminoAumento[i]] += cuelloBotella;
+                flujo[caminoAumento[i]][caminoAumento[i+1]] += cuelloBotella;
+                
+                //Bajo el flujo de la arista si no esta
+                flujo[caminoAumento[i+1]][caminoAumento[i]] -= cuelloBotella;
             }
         }
-        return flujo;
+        return cutCapacity();
     }
 
-     
+    int cutCapacity(){
+        int res = 0;
+        for(int vecino: adyacencias[0]){
+            res += flujo[0][vecino];
+        }
+        return res;
+    }    
     //Ademas construimos este camino si existiese
     vector<int> buildPath(vector<int> &padre,int end){
         caminoAumento.clear();
@@ -60,35 +65,10 @@ struct flow_digraph{
         }
         caminoAumento.push_back(0);
         reverse(caminoAumento.begin(),caminoAumento.end());
+        ;
         return caminoAumento;
     }
     
-   
-    vector<string> D_paths(int Pamount){
-       vector<string> paths; 
-       vector<vector<bool>> tomadas(adyacencias.size(),vector<bool>(adyacencias.size(),false));
-
-        for(int u : adyacencias[0]){
-            //Los caminos se indexan desde 1
-            if(rRes[u][0] == 1 && !tomadas[0][u]){
-                tomadas[0][u] = true;
-                paths.push_back("1" + T_DFS(u,tomadas));
-
-            }
-        }
-        return paths;
-        
-    }
-    string T_DFS(int root,vector<vector<bool>> &tomadas){
-        if(root == ogAdy.size()-1) return to_string(root+1);
-        for(int u : ogAdy[root]){
-            if(rRes[u][root] == 1 && !tomadas[root][u]){
-                //Los caminos se indexan desde uno
-                return to_string(root+ 1) + T_DFS(u,tomadas);
-
-            }
-        }
-    }
     
     bool hasAugmenthPath(){
         queue<int> q; 
@@ -104,7 +84,7 @@ struct flow_digraph{
             
                 
                 //Si es una arista valida del residual el valor es distinto de 0
-                if(not visitados[vecino] && rRes[v][vecino] != 0){
+                if(not visitados[vecino] && flujo[v][vecino] < capacidad[v][vecino]){
                 
                     padre[vecino] = v;
                     visitados[vecino] = true;
@@ -124,47 +104,67 @@ struct flow_digraph{
     int findBottleNeck(){
         int neck = INF;
         fore(i,0,caminoAumento.size()-1){
-            neck = min(neck,rRes[caminoAumento[i]][caminoAumento[i+1]]);
+            int p = caminoAumento[i];
+            int v = caminoAumento[i+1];
+            neck = min(neck,capacidad[p][v] - flujo[p][v]);
         }
         return neck;
+    }
+
+    vector<vector<int>> Paths(){ 
+        //Funcion para reocnstruir los caminos disjuntos en el grafo original
+        queue<int> q; 
+        vector<bool> visitados(adyacencias.size(),false);
+        vector<int> padre(adyacencias.size(),-1);
+        visitados[0] = true; q.push(0);
+        padre[0] = 0;
+        int sumidero = adyacencias.size()-1;
+        vector<vector<int>> caminos;
+        while(!q.empty()){
+            int v = q.front();q.pop();
+            for(int w : adyacenciasOG[v]){
+                if(w == sumidero){
+                    vector<int> temp = buildPath(padre,v);
+                    temp.push_back(sumidero);
+                    caminos.push_back(temp);
+                }
+                if(!visitados[w] && flujo[v][w] == 1){
+                    visitados[w] = true;
+                    padre[w] = v;
+                    q.push(w);
+                }
+                
+            
+                
+            }
+        }
+        return caminos;
     }
 };
 
 
-
 int main(){
-    
-    
-    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-   
-   
-    cin >> N >> M   ;
-
-    int tamañoGrafo = N;
-
-    flow_digraph G(tamañoGrafo);
-        
-    fore(i,0,M){
+    int n,m; 
+    cin >> n >> m;
+    flow_digraph G(n);
+    fore(i,0,m){
         int a,b;
         cin >> a >> b;
-        G.addPair(a-1,b-1);
-            
+        G.addEdge(a-1,b-1,1);
     }
-    int cant = G.maxFlow();
-    
-    vector<string> res =G.D_paths(cant);
-    cout << cant << "\n";
-    if(cant != 0){
-    for(auto p : res){
-        cout << p.size() << "\n";
-        for(char x:p){
+
+    int res = G.maxFlow();
+    cout << res << "\n";
+    if(res != 0){
+        vector<vector<int>> caminos = G.Paths();
+        
+        fore(i,0,caminos.size()){
+            cout << caminos[i].size() << "\n";
+            fore(j,0,caminos[i].size()){
+                cout << caminos[i][j] + 1    << " ";
             
-            cout << x << " ";
+            }
+            cout << "\n";
         }
-        cout << "\n";
     }
-    }   
-
 }
-
-
--- GraphProblems/Monsters.cpp
+++ GraphProblems/Monsters.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define fore(i,a,b) for(int i = a; i< b; i++)
 typedef long long ll ;
-const ll INF = 1e15;
+const int INF = 1e9;
 int n, m;
 
 
@@ -25,6 +25,8 @@ bool esBorde(int i,int j){
     return (j == 0 || j == m - 1) || (i == 0 || i == n -1);
 }
 
+
+
 int main(){
    
     cin >> n >> m;
@@ -42,14 +44,22 @@ int main(){
             if(esBorde(i,j) && temp == '.') {
                 bordes.push_back({i,j});
             }else if(temp == 'M') monstruos.push_back({i,j});
-            else if(temp == 'A') start = {i,j};
+            else if(temp == 'A'){
+                start = {i,j};
+                if(esBorde(i,j)){ 
+                    cout << "YES"; return 0;
+                }
+            }
+             
 
         }
        
     }
     
-    vector<vector<int>> D(n,vector<int>(m)); vector<vector<pair<int,int>> > P(n,vector<pair<int,int>>(m));
-    //BFS desde el inicio a todos
+    vector<vector<int>> D(n,vector<int>(m,INF)); vector<vector<pair<int,int>> > P(n,vector<pair<int,int>>(m));
+    
+    P[start.first][start.second] = start;
+   //BFS desde el inicio a todos
     D[start.first][start.second] = 0;
     queue<Node> q;
     Node inicio(start.first,start.second,0);
@@ -57,34 +67,99 @@ int main(){
     while(!q.empty()){
         Node act = q.front(); q.pop();
         //Abajo
-        if(isValid(act.y + 1,act.x) && D[act.y + 1][act.x]){
+        if(isValid(act.y + 1,act.x) && D[act.y + 1][act.x] == INF){
             D[act.y + 1][act.x] = act.lvl + 1;
             P[act.y + 1][act.x] = {act.y,act.x};
             q.push(Node(act.y+1,act.x, act.lvl + 1));
         }
         //Arriba
-        if(isValid(act.y -1 ,act.x) && D[act.y - 1][act.x]){
+        if(isValid(act.y -1 ,act.x) && D[act.y - 1][act.x] == INF){
             D[act.y - 1][act.x] = act.lvl + 1;
             P[act.y - 1][act.x] = {act.y,act.x};
             q.push(Node(act.y-1,act.x, act.lvl + 1));
         }
         //Derecha
-        if(isValid(act.y ,act.x + 1) && D[act.y][act.x + 1]){
+        if(isValid(act.y ,act.x + 1) && D[act.y][act.x + 1] == INF){
             D[act.y ][act.x + 1] = act.lvl + 1;
             P[act.y ][act.x + 1] = {act.y,act.x};
             q.push(Node(act.y,act.x + 1, act.lvl + 1));
         }
-        //Izqueierda
-        if(isValid(act.y,act.x -1) && D[act.y ][act.x-1]){
+        //Izquerda
+        if(isValid(act.y,act.x -1) && D[act.y ][act.x-1] == INF){
             D[act.y ][act.x - 1] = act.lvl + 1;
-             P[act.y ][act.x - 1] = {act.y,act.x};
+            P[act.y ][act.x - 1] = {act.y,act.x};
             q.push(Node(act.y,act.x - 1, act.lvl + 1));
         }
 
     }
     //BFS desde el super monstruo
-    vector<vector<int>> M(n,vector<int>(m));
+    vector<vector<int>> M(n,vector<int>(m,INF));
+    //Ponemos a todos los monstruos con distancia 0, como si la bfs hubiese empezado desde un nodo fantasma
+    fore(i,0,monstruos.size()){
+        int m_i = monstruos[i].first;
+        int m_j = monstruos[i].second;
+        M[m_i][m_j] = 0;
+        q.push(Node(m_i,m_j,0));
+       
+    }
     
+    while(!q.empty()){
+        Node act = q.front(); q.pop();
+        //Abajo
+        if(isValid(act.y + 1,act.x) && M[act.y + 1][act.x] == INF){
+            M[act.y + 1][act.x] = act.lvl + 1;
+            
+            q.push(Node(act.y+1,act.x, act.lvl + 1));
+        }
+        //Arriba
+        if(isValid(act.y -1 ,act.x) && M[act.y - 1][act.x] == INF){
+            M[act.y - 1][act.x] = act.lvl + 1;
+            
+            q.push(Node(act.y-1,act.x, act.lvl + 1));
+        }
+        //Derecha
+        if(isValid(act.y ,act.x + 1) && M[act.y][act.x + 1] == INF){
+            M[act.y ][act.x + 1] = act.lvl + 1;
+            
+            q.push(Node(act.y,act.x + 1, act.lvl + 1));
+        }
+        //Izquerda
+        if(isValid(act.y,act.x -1) && M[act.y ][act.x-1] == INF){
+            M[act.y ][act.x - 1] = act.lvl + 1;
+            
+            q.push(Node(act.y,act.x - 1, act.lvl + 1));
+        }
+        
+        
+    }
 
 
+    //Nos fijamos si la distancia a alguna salida es menor que la de los monstruos
+    vector<char> path;
+    fore(i,0,bordes.size()){
+        int sX,sY; tie(sY,sX) = bordes[i];
+        //Reconstruimos el camino
+        if(D[sY][sX] < M[sY][sX]){
+            cout << "YES" << "\n";
+            for( pair<int,int> v = {sY,sX}; v != P[v.first][v.second]; v = P[v.first][v.second]){
+                //Bajo
+                if(P[v.first][v.second].first < v.first) path.push_back('D');
+                //Subio
+                if(P[v.first][v.second].first > v.first) path.push_back('U');
+                //Derecha
+                if(P[v.first][v.second].second < v.second) path.push_back('R');
+                //Izquierda
+                if(P[v.first][v.second].second > v.second) path.push_back('L');
+
+            }
+            cout << path.size() << "\n";
+            reverse(path.begin(),path.end());
+
+            for(char Letra : path){
+                cout << Letra;
+            }
+            return 0;
+        }
+    }
+    cout << "NO";
 }
